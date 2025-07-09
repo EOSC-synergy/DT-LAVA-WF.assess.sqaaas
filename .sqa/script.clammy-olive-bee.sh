35d457d6336cb6d6a624f1a9acca09ef3e6a7ba1
Pablo Orviz <orviz@ifca.unican.es>
2025-07-09 17:10:10 +0200

    Add JePL file structure

---
+++ .sqa/script.clammy-olive-bee.sh
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: Copyright contributors to the Software Quality Assurance as a Service (SQAaaS) project <sqaaas@ibergrid.eu>
+# SPDX-FileContributor: Pablo Orviz <orviz@ifca.unican.es>
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+(
+cd gitlab.com/louise.cordrie/DT-LAVA-WF/DTC53/WF5301/ST530101/src &&
+    licensee detect . --confidence 60 --json 
+)
---
+++ .sqa/script.craggy-amaranth-toad.sh
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: Copyright contributors to the Software Quality Assurance as a Service (SQAaaS) project <sqaaas@ibergrid.eu>
+# SPDX-FileContributor: Pablo Orviz <orviz@ifca.unican.es>
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+(
+cd gitlab.com/louise.cordrie/DT-LAVA-WF/DTC53/WF5301/ST530101/src &&
+    bandit -f json --quiet  --confidence-level high --severity-level high --recursive  .
+)
